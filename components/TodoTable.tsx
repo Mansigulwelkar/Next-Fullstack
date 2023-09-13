ef21a45b30d8f48c733de2e5efeebacec957ca0c
maheshh <mahesh@infogird.com>
2023-09-13 09:52:18 +0530

    initial commit

---
+++ components/TodoTable.tsx
@@ -0,0 +1,58 @@
+"use client"
+import { useEffect, useState } from 'react'
+type TODO = {
+    _id?: string,
+    task: string,
+    desc: string,
+    createAt?: string,
+    updateAt?: string,
+    _v?: string
+}
+
+const TodoTable = () => {
+
+    const [allTodos, setAllTodos] = useState<TODO[]>([])
+    const getTodo = async () => {
+        const res = await fetch("/api/todo", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const { result } = await res.json()
+        console.log(result);
+        setAllTodos(result)
+
+    }
+    useEffect(() => {
+        getTodo()
+    }, [])
+
+    const deleteTodo = async () => {
+        const [deleted, setDeleted] = useState([])
+        const res = await fetch("/api/todo", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const { result } = await res.json()
+        console.log(result);
+        setDeleted(result)
+
+    }
+    useEffect(() => {
+        deleteTodo()
+    }, [])
+
+    return <>
+        {
+            allTodos.map(item => <div className='flex gap-5 '>
+                <h1> {item.task}</h1>
+                <button >delete</button>
+            </div>)
+        }
+    </>
+}
+
+export default TodoTable
